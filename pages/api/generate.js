f5440dd8512aace711093a59eac65170f7a4b5c9
Frank Young <frankyoung@M-C02DD0HZML86.routerlogin.net>
2022-11-22 15:02:55 -0800

    non functioning slider

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,7 +9,8 @@ export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
     prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    temperature: 0.7,
+    max_tokens: 256,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
@@ -17,12 +18,16 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+    return `Create an insult for you or your mama. 
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+    Adjective: ugly
+    Insult: Your mama so ugly, goats give her makeup advice
+    Adjective: fat
+    Insult: Your mama so fat, she warps spacetime
+    Adjective: dumb
+    Insult: Your mama so dumb, she thought Steph Curry was an Indian dish
+  
+  Adjective: ${capitalizedAnimal}
+  Insult:`;
+  
 }
--- pages/index.js
+++ pages/index.js
@@ -23,22 +23,25 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>Yo mama so...</title>     
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Yo mama so...</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter an adjective; ie. ugly, fat, stupid"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <p>Rawness</p>
+          <input type="range" id="rawness" name="rawness" min="1" max="5" step="1" />
+          <input type="submit" value="Start cappin" />
+          
         </form>
         <div className={styles.result}>{result}</div>
       </main>
